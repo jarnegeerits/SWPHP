d492a30d234c9aa55ce6b8b693fe67e8d37a9f52
jarnegeerits <jarne.ge@gmail.com>
2020-06-06 21:16:18 +0200

    login done

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\eindwerk\SWPHP\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\eindwerk\SWPHP\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\SocialWheels\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\SocialWheels\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'LandingController@landingLoad');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
