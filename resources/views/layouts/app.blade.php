a97076623ac3367e349902b1b9d032faf8f0b3e1
jarnegeerits <jarne.ge@gmail.com>
2020-06-14 02:24:39 +0200

    navigation updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,15 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand navhover" href="{{ url('/') }}">
-                    {{ config('app.name', 'SocialWheels') }}
-                </a>
+                @if (Auth::check())
+                    <a class="navbar-brand navhover" href="{{ url('/home') }}">
+                        {{ config('app.name', 'SocialWheels') }}
+                    </a>
+                @else
+                    <a class="navbar-brand navhover" href="{{ url('/') }}">
+                        {{ config('app.name', 'SocialWheels') }}
+                    </a>
+                @endif
                 {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button> --}}
